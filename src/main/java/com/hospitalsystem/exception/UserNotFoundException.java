0adbcfa62e27ece20549da0ae790405bd5f691ee
teera12 <bbkonkayeva@gmail.com>
2022-11-21 17:02:28 +0600

    Hospital

---
+++ src/main/java/com/hospitalsystem/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hospitalsystem.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(Long id){
+        super("Could not found the user with id "+ id);
+    }
+}
---
+++ src/test/java/com/hospitalsystem/HospitalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
